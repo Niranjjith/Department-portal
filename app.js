58275af99b3cd376c629919f767f0f8d8f24ce00
Niranjan A R <niranjjithbathery@gmail.com>
2025-12-10 13:39:35 +0530

    Updated some features

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error("❌ MongoDB connection error:", err.message));
 
 // ----------------- Middleware ----------------------
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
